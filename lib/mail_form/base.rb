3c42ea12e6de5d740d32d3785b561989d5b1ec75
Rene <reneescobar07@gmail.com>
2014-07-12 01:17:32 -0400

    created the active record model for the mail form

---
+++ lib/mail_form/base.rb
@@ -0,0 +1,55 @@
+module MailForm
+	class Base
+		class_attribute :attribute_names
+		self.attribute_names = []
+		include ActiveModel::Conversion
+		extend ActiveModel::Naming
+		extend ActiveModel::Translation
+		include ActiveModel::AttributeMethods
+		include ActiveModel::Validations
+
+		attribute_method_prefix 'clear_'
+
+		attribute_method_suffix '?'
+
+		def self.attributes(*names)
+			attr_accessor(*names)
+			define_attribute_methods(names)
+			self.attribute_names += names
+		end
+
+		def persisted?
+			false
+		end
+
+		def deliver
+			if valid? 
+				MailForm::Notifier.contact(self).deliver
+			else
+				false
+			end
+		end
+
+		protected 
+
+		def clear_attribute(attribute)
+			send("#{attribute}=", nil)
+		end
+
+		def attribute?(attribute)
+			send(attribute).present?
+		end
+		# def method_missing(method, *args, &block)
+		# if the instance of User class doesn't respond to an attribute
+		# the raise the method_missing error 
+		#   if respond_to_without_attributes?(method, true)
+		#     super
+		#   else
+		# turn the method into a string, :name => 'name'
+		#     match = match_attribute_method?(method.to_s)
+		# 
+		#     match ? attribute_missing(match, *args, &block) : super
+		#   end
+		# end
+	end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1d1834c8413047d91bc13b7717a7d6dbcad3d755d9959478c356240792a89720c8859ca138abc8e5cca95a3bac7a236624c4dae0346c75b5fd47a192c995aa89'
